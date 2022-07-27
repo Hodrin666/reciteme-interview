b562256e4bdfe244ec953b3dac20611c4513eb67
Pedro Sobral <pedrosobral666.uk@gmail.com>
2022-07-27 16:25:16 +0100

    Feature get all posts

---
+++ src/hooks/getWindowSize.ts
@@ -0,0 +1,27 @@
+/**
+ * Module dependencies.
+ */
+
+import { useEffect, useState } from 'react';
+
+export default function useWindowDimensions() {
+	const [windowDimensions, setWindowDimensions] = useState({
+		height: 0,
+		width: 0,
+	});
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowDimensions({
+				height: window.innerHeight,
+				width: window.innerWidth,
+			});
+		}
+
+		handleResize();
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	return windowDimensions;
+}
---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+export const size = {
+	l: '1440',
+	m: '1024',
+	s: '425',
+	xl: '2560',
+	xm: '768',
+	xs: '375',
+	xxs: '320',
+};
+
+export const device = {
+	l: `(min-width: ${size.l}px)`,
+	m: `(min-width: ${size.m}px)`,
+	s: `(min-width: ${size.s}px)`,
+	xl: `(min-width: ${size.xl}px)`,
+	xm: `(min-width: ${size.xm}px)`,
+	xs: `(min-width: ${size.xs}px)`,
+	xxs: `(min-width: ${size.xxs}px)`,
+};
